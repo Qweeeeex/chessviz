4ad5d9cd92890314e87fd7dd4f7d8d321d1f49bb
Qweeeeex <vbvbvbvb72@gmail.com>
2020-05-17 22:44:58 +0700

    Update source.cpp

--- src/source.cpp
+++ src/source.cpp
@@ -1,224 +1,127 @@
 #include "header.h"
-#include <iomanip>
+#include <cstring>
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int move_check(int x1, int x2, int y1, int y2, char pole[][9], string step)
+using namespace std;
+
+int x1 = 0, x2 = 0;
+int y1 = 0, y2 = 0;
+int checking;
+
+void move(char mowement[], char board[][9])
 {
-    int flag = 0;
-    if ((x1 == x2) && (x1 == y2)) {
-        cout << "\nERROR\n";
-        return 0;
-    }
-    switch (pole[x1][y1]) {
-    case ' ':
-        cout << "\nERROR\n";
-        return 0;
-
-    case 'P':
-        if ((y1 == y2) && (x1 - x2 == 1) && (pole[x2][y2] == ' '))
-            flag = 1;
-        if ((y1 == y2) && (x1 - x2 == 2) && (x1 == 6)
-            && (pole[x2][y2] == ' ')) {
-            if (pole[x2 + 1][y2] == ' ') {
-                flag = 1;
-            } else {
-                cout << "\nERROR\n";
-                return 0;
-            }
+    if (strlen(mowement) == 5) {
+        checking = 0;
+        if (editb(mowement, board, checking)) {
+            movePP(board);
         }
-        if ((x1 - x2 == 1) && (pole[x2][x2] != ' ')
-            && ((y1 - y2 == 1) || (y2 - y1 == 1)))
-            flag = 1;
-        break;
-
-    case 'p':
-        if ((y1 == y2) && (x2 - x1 == 1) && (pole[x2][y2] == ' '))
-            flag = 1;
-        if ((y1 == y2) && (x2 - x1 == 2) && (x1 == 1)
-            && (pole[x2][y2] == ' ')) {
-            if (pole[x2 - 1][y2] == ' ') {
-                flag = 1;
-            } else {
-                cout << "\nERROR\n";
-                return 0;
-            }
-        }
-        if ((x2 - x1 == 1) && (pole[x2][y2] != ' ')
-            && ((y2 - y1 == 1) || (y1 - y2 == 1)))
-            flag = 1;
-        break;
-
-    default:
-        flag = 1;
-        break;
-    }
-    if (pole[x1][y1] != 'P' && pole[x1][y1] != 'p') {
-        cout << "Error! Try again" << endl;
-        return 0;
-    }
-
-    if (step[2] == '-') {
-        if (pole[x2][y2] == ' ') {
-        } else {
-            cout << "Incorrect" << endl;
-            return 0;
-        }
-    } else if (step[2] == 'x') {
-        if (pole[x2][y2] != ' ') {
-            flag = 1;
-            return flag;
-        } else {
-            cout << "Incorrect" << endl;
-            return 0;
+    } else {
+        checking = 1;
+        if (editb(mowement, board, checking)) {
+            moveP(mowement, board);
         }
     }
-
-    return flag;
 }
 
-void move_p(char pole[][9])
+void movePP(char board[][9])
 {
-    int x1 = 9, x2 = 9, y1 = 0, y2 = 0;
-    string step;
-    cout << endl << "Your move: ";
-    cin >> step;
-    if (step == "exit") {
-        cout << "Exit the programm";
-        exit(0);
+    if (board[y1 - 1][x1] == 'p') {
+        board[y1 - 1][x1] = ' ';
+        board[y2 - 1][x2] = 'p';
+    } else if (board[y1 - 1][x1] == 'P') {
+        board[y1 - 1][x1] = ' ';
+        board[y2 - 1][x2] = 'P';
+    } else {
+        cout << "Incorrect input. This not a pawn" << endl << endl;
     }
-    if (step.length() != 5) {
-        cout << "\nERROR\n";
-        return;
+}
+
+void moveP(char mowement[], char board[][9])
+{
+    if (board[y1 - 1][x1] == mowement[0]) {
+        board[y1 - 1][x1] = ' ';
+        board[y2 - 1][x2] = mowement[0];
     }
-    switch (step[0]) {
+}
+
+bool editb(char mowement[], char board[][9], int checking)
+{
+    string transformIntoInt = " ";
+    int outOfBoard1 = 0, outOfBoard2 = 9;
+    switch (mowement[0 + checking]) {
     case 'a':
-        y1 = 1;
-        break;
-    case 'b':
-        y1 = 2;
-        break;
-    case 'c':
-        y1 = 3;
-        break;
-    case 'd':
-        y1 = 4;
-        break;
-    case 'e':
-        y1 = 5;
-        break;
-    case 'f':
-        y1 = 6;
-        break;
-    case 'g':
-        y1 = 7;
-        break;
-    case 'h':
-        y1 = 8;
-        break;
-    default:
-        cout << "\nERROR\n";
-        return;
-    }
-    switch (step[1]) {
-    case '1':
-        x1 = 7;
-        break;
-    case '2':
-        x1 = 6;
-        break;
-    case '3':
-        x1 = 5;
-        break;
-    case '4':
-        x1 = 4;
-        break;
-    case '5':
-        x1 = 3;
-        break;
-    case '6':
-        x1 = 2;
-        break;
-    case '7':
         x1 = 1;
         break;
-        x1 = 0;
-        break;
-    default:
-        cout << "\nERROR\n";
-        return;
-    }
-
-    switch (step[3]) {
-    case 'a':
-        y2 = 1;
-        break;
     case 'b':
-        y2 = 2;
+        x1 = 2;
         break;
     case 'c':
-        y2 = 3;
+        x1 = 3;
         break;
     case 'd':
-        y2 = 4;
+        x1 = 4;
         break;
     case 'e':
-        y2 = 5;
+        x1 = 5;
         break;
     case 'f':
-        y2 = 6;
+        x1 = 6;
         break;
     case 'g':
-        y2 = 7;
+        x1 = 7;
         break;
     case 'h':
-        y2 = 8;
+        x1 = 8;
         break;
-    default:
-        cout << "\nERROR\n";
-        return;
-    }
-    switch (step[4]) {
-    case '1':
-        x2 = 7;
-        break;
-    case '2':
-        x2 = 6;
-        break;
-    case '3':
-        x2 = 5;
-        break;
-    case '4':
-        x2 = 4;
-        break;
-    case '5':
-        x2 = 3;
-        break;
-    case '6':
-        x2 = 2;
-        break;
-    case '7':
-        x2 = 1;
-        break;
-    case '8':
-        x2 = 0;
-        break;
-    default:
-        cout << "\nERROR\n";
-        return;
+    default: {
+        cout << "Incorrect input" << endl << endl;
+        return false;
+    }; break;
     }
 
-    if (move_check(x1, x2, y1, y2, pole, step)) {
-        if (pole[x1][y1] == 'P') {
-            pole[x2][y2] = 'P';
-            pole[x1][y1] = ' ';
-        } else if (pole[x1][y1] == 'p') {
-            pole[x2][y2] = 'p';
-            pole[x1][y1] = ' ';
-        }
+    switch (mowement[3 + checking]) {
+    case 'a':
+        x2 = 1;
+        break;
+    case 'b':
+        x2 = 2;
+        break;
+    case 'c':
+        x2 = 3;
+        break;
+    case 'd':
+        x2 = 4;
+        break;
+    case 'e':
+        x2 = 5;
+        break;
+    case 'f':
+        x2 = 6;
+        break;
+    case 'g':
+        x2 = 7;
+        break;
+    case 'h':
+        x2 = 8;
+        break;
+    default: {
+        cout << "Incorrect input" << endl << endl;
+        return false;
+    }; break;
     }
-    cout << endl;
-    cout << endl;
+    transformIntoInt[0] = mowement[1 + checking];
+    y1 = atoi(transformIntoInt.c_str());
+
+    transformIntoInt[0] = mowement[4 + checking];
+    y2 = atoi(transformIntoInt.c_str());
+
+    if (y1 <= outOfBoard1 || y1 >= outOfBoard2 || x1 <= outOfBoard1
+        || x1 >= outOfBoard2 || x2 <= outOfBoard1 || x2 >= outOfBoard2
+        || y2 <= outOfBoard1 || y2 >= outOfBoard2
+        || (board[y1 - 1][x1] == ' ')) {
+        cout << "Incorrect input. Abroad board or empty cage" << endl;
+        return false;
+    }
+    return true;
 }
